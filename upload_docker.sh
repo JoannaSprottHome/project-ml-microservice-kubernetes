071d46e8b6be346c1195f2751922dfac213976e8
JoannaSprottHome <joanna@192.168.1.111>
2020-08-20 22:39:54 +0100

    Add docker commands to upload to ECR

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=joannasprott
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 310907231111.dkr.ecr.us-west-2.amazonaws.com
+docker tag mlpredict:latest 310907231111.dkr.ecr.us-west-2.amazonaws.com/mlpredict:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push 310907231111.dkr.ecr.us-west-2.amazonaws.com/mlpredict:latest
