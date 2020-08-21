75c4c54fd8b0d7c21fc2c6a2b8be7b7f8bae7f49
JoannaSprottHome <JoannaSprottHome@users.noreply.github.com>
2020-08-21 22:36:00 +0100

    Complete run kubernetes script and update docker upload to use docker. (#5)
    * Complete run_kubernetes script and update docker upload to use docker hub
    
    Co-authored-by: JoannaSprottHome <joanna@joannas-mbp.mynet>

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,21 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="joannasprott/mlpredict"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run mlpredict\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=8000 --labels app=mlpredict
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward mlpredict 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,14 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=joannasprott
+dockerpath="joannasprott/mlpredict"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 310907231111.dkr.ecr.us-west-2.amazonaws.com
-docker tag mlpredict:latest 310907231111.dkr.ecr.us-west-2.amazonaws.com/mlpredict:latest
+cat ~/docker-pass.txt | docker login --username=joannasprott --password-stdin
+docker tag mlpredict:latest joannasprott/mlpredict:latest
 
 # Step 3:
 # Push image to a docker repository
-docker push 310907231111.dkr.ecr.us-west-2.amazonaws.com/mlpredict:latest
+docker push joannasprott/mlpredict:latest
